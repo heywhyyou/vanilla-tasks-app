a6c1259ced47ee9aaaf65e2a1da06f157f042e99
heywhyyou <havenoreasontolive@gmail.com>
2023-11-15 22:21:25 +0300

    feat: improve addTask (implement addToStorage)

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+// Logic task
+
+const STATUS = {
+  TODO: "to do",
+  DONE: "done",
+};
+
+const PRIORITY = {
+  LOW: "low",
+  HIGH: "high",
+};
+
+const tasks = new Array();
+
+// const checkNameValidity = (name) => {
+//   return typeof name === "string" && name !== "";
+// };
+
 const chooseList = (event) => {
   return event.target.id === "high-form" ? "#high-tasks" : "#low-tasks";
 };
@@ -6,7 +24,14 @@ const choosePriority = (event) => {
   return event.target.id === "high-form" ? "#new-task-high" : "#new-task-low";
 };
 
-function addTask(event) {
+function addToStorage(event, name, status) {
+  let priority = choosePriority(event) === "#new-task-high" ? "high" : "low";
+  console.log(priority);
+  console.log(event);
+  tasks.push({ name, status, priority });
+}
+
+function addTask(event, status = STATUS.TODO) {
   event.preventDefault();
   let inputValue = document.querySelector(choosePriority(event)).value;
   let newTask = document.createElement("li");
@@ -27,6 +52,8 @@ function addTask(event) {
 
   taskList.appendChild(newTask);
   event.target.reset();
+  addToStorage(event, inputValue, status);
+  console.log(tasks);
 }
 
 let highForm = document.querySelector("#high-form");
